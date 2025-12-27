19e41b374615a82ee77c88dfe1a2b246605935a7
Sirome TV <mdsiromehd@gmail.com>
2025-12-27 16:50:28 +0600

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// src/main.js - এখানে আপনার সমস্ত জাভাস্ক্রিপ্ট কোড থাকবে
+
+// Firebase Configuration
+const firebaseConfig = window.firebaseConfig || {
+  apiKey: "AIzaSyBL6nN2TH4uyp8iso6Q99_QHkYyHnslXjU",
+  authDomain: "tg-bot-sirome-tv.firebaseapp.com",
+  databaseURL: "https://tg-bot-sirome-tv-default-rtdb.firebaseio.com",
+  projectId: "tg-bot-sirome-tv",
+  storageBucket: "tg-bot-sirome-tv.firebasestorage.app",
+  messagingSenderId: "764854962010",
+  appId: "1:764854962010:web:6a77f650622614de4d5563",
+  measurementId: "G-8G48FV41TH"
+};
+
+// বাকি সব জাভাস্ক্রিপ্ট কোড এখানে থাকবে...
+// Playlist URLs, Firebase functions, loadChannels(), ইত্যাদি
